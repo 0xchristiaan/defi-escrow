7063f040e02b84ab7da456a374e5714098dca061
christiaan koopal <0xchristiaan@gmail.com>
2022-10-22 17:54:39 +0200

    Escrow Update

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,34 +1,91 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./EscrowNFT.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Escrow is Ownable {
 
-    event Withdrawal(uint amount, uint when);
+    EscrowNFT public escrowNFT;
+    bool public initialized = false;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    event Escrowed(address _from, address _to, uint256 _amount, uint256 _matureTime);
+    event Redeemed(address _recipient, uint256 _amount);
+    event Initialized(address _escrowNft);
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    modifier isInitialized() {
+        require(initialized, "Contract is not yet initialized");
+        _;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function initialize(address _escrowNftAddress) external onlyOwner {
+        require(!initialized, "Contract already initialized.");
+        escrowNFT = EscrowNFT(_escrowNftAddress);
+        initialized = true;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+        emit Initialized(_escrowNftAddress);
     }
+
+    function escrowEth(address _recipient, uint256 _duration) external payable isInitialized {
+        require(_recipient != address(0), "Cannot escrow to zero address.");
+        require(msg.value > 0, "Cannot escrow 0 ETH.");
+
+        uint256 amount = msg.value;
+        uint256 matureTime = block.timestamp + _duration;
+
+        escrowNFT.mint(_recipient, amount, matureTime);
+
+        emit Escrowed(msg.sender,
+            _recipient,
+            amount,
+            matureTime);
+    }
+
+    function redeemEthFromEscrow(uint256 _tokenId) external isInitialized {
+        require(escrowNFT.ownerOf(_tokenId) == msg.sender, "Must own token to claim underlying Eth");
+
+        (uint256 amount, uint256 matureTime) = escrowNFT.tokenDetails(_tokenId);
+        require(matureTime <= block.timestamp, "Escrow period not expired.");
+
+        escrowNFT.burn(_tokenId);
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+
+        require(success, "Transfer failed.");
+
+        emit Redeemed(msg.sender, amount);
+    }
+
+
+    function redeemAllAvailableEth() external isInitialized {
+        uint256 nftBalance = escrowNFT.balanceOf(msg.sender);
+        require(nftBalance > 0, "No EscrowNFTs to redeem.");
+
+        uint256 totalAmount = 0;
+
+        for (uint256 i = 0; i < nftBalance; i++) {
+            uint256 tokenId = escrowNFT.tokenOfOwnerByIndex(msg.sender, i);
+            (uint256 amount, uint256 matureTime) = escrowNFT.tokenDetails(tokenId);
+
+            if (matureTime <= block.timestamp) {
+                escrowNFT.burn(tokenId);
+                totalAmount += amount;
+            }
+        }
+
+        require(totalAmount > 0, "No Ether to redeem.");
+
+        (bool success, ) = msg.sender.call{value: totalAmount}("");
+
+        require(success, "Transfer failed.");
+
+        emit Redeemed(msg.sender, totalAmount);
+    }
+
+    function contractAddress() public view returns (address) {
+        return address(this);
+    }
+
 }
