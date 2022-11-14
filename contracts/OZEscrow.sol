58f4ce75bf9aa37112f0ec271c4e9706faccb300
christiaan koopal <0xchristiaan@gmail.com>
2022-11-14 22:29:04 +0100

    OpenZeppelin Escrow

---
+++ contracts/OZEscrow.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/utils/escrow/Escrow.sol";
+import "@openzeppelin/contracts/utils/escrow/ConditionalEscrow.sol";
+
+contract Escrow is ConditionalEscrow{
+    // implement AAVE logic
+
+}
+
